249a97e52e8486850d2da66a980d22e6365a2741
Reisorz <166374263+Reisorz@users.noreply.github.com>
2024-11-18 15:43:40 +0100

    First commit

---
+++ src/test/java/com/mls/Expense_Tracker_API/ExpenseTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mls.Expense_Tracker_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
