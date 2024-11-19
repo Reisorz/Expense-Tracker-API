a03f3b4504a096ffdee238d2722dc35def4b4486
Reisorz <166374263+Reisorz@users.noreply.github.com>
2024-11-19 11:10:56 +0100

    Token and User entities.

---
+++ src/main/java/com/mls/Expense_Tracker_API/auth/repository/Token.java
@@ -0,0 +1,43 @@
+package com.mls.Expense_Tracker_API.auth.repository;
+
+import com.mls.Expense_Tracker_API.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tokens")
+public final class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private TokenType tokenType = TokenType.BEARER;
+
+    @Column(nullable = false)
+    private Boolean isRevoked;
+
+    @Column(nullable = false)
+    private Boolean isExpired;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    public enum TokenType {
+        BEARER
+    }
+}
---
+++ src/main/java/com/mls/Expense_Tracker_API/user/User.java
@@ -0,0 +1,35 @@
+package com.mls.Expense_Tracker_API.user;
+
+import com.mls.Expense_Tracker_API.auth.repository.Token;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Token> tokens;
+}
