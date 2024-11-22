debd06d4515e1ed8ea7c1dc96145c42edc7ffb26
Reisorz <166374263+Reisorz@users.noreply.github.com>
2024-11-22 10:03:11 +0100

    Completing SecurityConfig.

--- src/main/java/com/mls/Expense_Tracker_API/auth/config/SecurityConfig.java
+++ src/main/java/com/mls/Expense_Tracker_API/auth/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.mls.Expense_Tracker_API.auth.config;
 
+import com.mls.Expense_Tracker_API.user.User;
+import com.mls.Expense_Tracker_API.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,17 +16,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserService userService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -34,7 +47,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests( http -> {
                     //Public endpoints
-                    http.requestMatchers(HttpMethod.POST, "/auth/**").permitAll();
+                    http.requestMatchers(HttpMethod.POST, "/auth/register").permitAll();
 
                     //Private endpoints
 
@@ -42,5 +55,65 @@ public class SecurityConfig {
                     http.anyRequest().denyAll();
                 })
                 .build();
+    }
+
+    //Config to allow crossOrigin
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:4200");  // Origin definition
+        config.addAllowedMethod("*");  // Allow all Http methods (PUT, POST, GET, etc.)
+        config.addAllowedHeader("*");  // Allow all headers
+        config.setAllowCredentials(true);  // Allows credentials
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);  // Applies CORS configuration to all routes
+
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            final User user = userService.findByEmail(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+
+              //Add roles and permissions later
+//            List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
+//
+//            userEntity.getRoles().forEach(role -> authorityList.add(new SimpleGrantedAuthority("ROLE_".concat(role.getRoleEnum().name()))));
+//
+//            userEntity.getRoles().stream().flatMap(role -> role.getPermissionList().stream())
+//                    .forEach(permission -> authorityList.add(new SimpleGrantedAuthority(permission.getPermissionName())));
+
+
+            return org.springframework.security.core.userdetails.User
+                    .builder()
+                    .username(user.getEmail())
+                    .password(user.getPassword())
+//                    .authorities(authorityList)
+                    .build();
+        };
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        //return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/java/com/mls/Expense_Tracker_API/user/UserService.java
+++ src/main/java/com/mls/Expense_Tracker_API/user/UserService.java
@@ -3,6 +3,8 @@ package com.mls.Expense_Tracker_API.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -11,5 +13,9 @@ public class UserService {
 
     public  User saveUser(User user) {
         return userRepository.save(user);
+    }
+
+    public Optional<User> findByEmail(String email){
+        return userRepository.findByEmail(email);
     }
 }
